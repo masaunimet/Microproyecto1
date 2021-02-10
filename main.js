16f16ebbe01f101f3a354f7f2ca9f7c0761e77ce
Tony <t27302013@correo.unimet.edu.ve>
2021-02-09 20:50:33 -0400

    basico de Skills

--- main.js
+++ main.js
@@ -14,6 +14,15 @@ let imagenes=[{
     descripcion:"Tony Cunsolo siempre sabra cual es la mejor broma que se tiene que hacer en el momento, aunque el mundo se este derrumbando, siempre es una buena compañia"
 }];
 
+let estadisticas = {
+    Inteligencia:"8/10",
+    Carisma:"7/10",
+    Fuerza:"6/10",
+    Destreza:"4/10",
+    Agilidad:"3/10",
+    Suerte:"10/10",
+};
+
 let contador =0;
 let atras = document.getElementById('boton_atras');
 let adelante = document.getElementById('boton_adelante');
@@ -21,6 +30,44 @@ let imagen_hero = document.getElementById('imagen_hero');
 let titulo_hero = document.getElementById('titulo_hero');
 let descripcion_hero = document.getElementById('descripcion_hero');
 
+let puntuacion = document.getElementById('puntuacion');
+let btn_inteligencia = document.getElementById('btn_inteligencia');
+let btn_carisma = document.getElementById('btn_carisma');
+let btn_fuerza = document.getElementById('btn_fuerza');
+let btn_destreza = document.getElementById('btn_destreza');
+let btn_suerte = document.getElementById('btn_suerte');
+let btn_agilidad = document.getElementById('btn_agilidad');
+
+
+btn_inteligencia.addEventListener('click',()=>{
+
+    puntuacion.innerHTML = estadisticas.Inteligencia;
+})
+
+btn_carisma.addEventListener('click',()=>{
+
+    puntuacion.innerHTML = estadisticas.Carisma;
+})
+
+btn_fuerza.addEventListener('click',()=>{
+
+    puntuacion.innerHTML = estadisticas.Fuerza;
+})
+
+btn_destreza.addEventListener('click',()=>{
+
+    puntuacion.innerHTML = estadisticas.Destreza;
+})
+
+btn_agilidad.addEventListener('click',()=>{
+
+    puntuacion.innerHTML = estadisticas.Agilidad;
+})
+
+btn_suerte.addEventListener('click',()=>{
+
+    puntuacion.innerHTML = estadisticas.Suerte;
+})
 
 adelante.addEventListener('click',()=>{
 
